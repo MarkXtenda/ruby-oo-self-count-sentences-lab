b8565090ec35f55f91987f86fd486b761b260202
MarkXtenda <mark.i2p.1998@gmail.com>
2022-08-09 20:43:27 +1000

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = []
+    self.split(/[.!?]/).each do |sentence|
+      (!sentence.empty?) ? sentences << sentence : nil
+    end
+    sentences.size
   end
 end
+
+new = String.new("new")
+
+puts new.sentence?
